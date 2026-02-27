e5a15aa482a4f143c41e19d3c946af069b7bf31d
Hongjungi <101107394+RedJungi@users.noreply.github.com>
2026-02-27 16:30:39 +0900

    Revert "계정관리 메뉴얼 업데이트"

--- docs/_static/sphinx_highlight.js
+++ docs/_static/sphinx_highlight.js
@@ -1,11 +1,7 @@
 /* Highlighting utilities for Sphinx HTML documentation. */
 "use strict";
 
-<<<<<<< HEAD
-const SPHINX_HIGHLIGHT_ENABLED = true
-=======
 const SPHINX_HIGHLIGHT_ENABLED = true;
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
 
 /**
  * highlight a given string on a node by wrapping it in
@@ -17,15 +13,9 @@ const _highlight = (node, addItems, text, className) => {
     const parent = node.parentNode;
     const pos = val.toLowerCase().indexOf(text);
     if (
-<<<<<<< HEAD
-      pos >= 0 &&
-      !parent.classList.contains(className) &&
-      !parent.classList.contains("nohighlight")
-=======
       pos >= 0
       && !parent.classList.contains(className)
       && !parent.classList.contains("nohighlight")
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
     ) {
       let span;
 
@@ -40,17 +30,7 @@ const _highlight = (node, addItems, text, className) => {
 
       span.appendChild(document.createTextNode(val.substr(pos, text.length)));
       const rest = document.createTextNode(val.substr(pos + text.length));
-<<<<<<< HEAD
-      parent.insertBefore(
-        span,
-        parent.insertBefore(
-          rest,
-          node.nextSibling
-        )
-      );
-=======
       parent.insertBefore(span, parent.insertBefore(rest, node.nextSibling));
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
       node.nodeValue = val.substr(0, pos);
       /* There may be more occurrences of search term in this node. So call this
        * function recursively on the remaining fragment.
@@ -60,11 +40,7 @@ const _highlight = (node, addItems, text, className) => {
       if (isInSVG) {
         const rect = document.createElementNS(
           "http://www.w3.org/2000/svg",
-<<<<<<< HEAD
-          "rect"
-=======
           "rect",
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
         );
         const bbox = parent.getBBox();
         rect.x.baseVal.value = bbox.x;
@@ -83,11 +59,7 @@ const _highlightText = (thisNode, text, className) => {
   let addItems = [];
   _highlight(thisNode, addItems, text, className);
   addItems.forEach((obj) =>
-<<<<<<< HEAD
-    obj.parent.insertAdjacentElement("beforebegin", obj.target)
-=======
     obj.parent.insertAdjacentElement("beforebegin", obj.target),
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
   );
 };
 
@@ -95,34 +67,15 @@ const _highlightText = (thisNode, text, className) => {
  * Small JavaScript module for the documentation.
  */
 const SphinxHighlight = {
-<<<<<<< HEAD
-
-=======
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
   /**
    * highlight the search words provided in localstorage in the text
    */
   highlightSearchWords: () => {
-<<<<<<< HEAD
-    if (!SPHINX_HIGHLIGHT_ENABLED) return;  // bail if no highlight
-=======
     if (!SPHINX_HIGHLIGHT_ENABLED) return; // bail if no highlight
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
 
     // get and clear terms from localstorage
     const url = new URL(window.location);
     const highlight =
-<<<<<<< HEAD
-        localStorage.getItem("sphinx_highlight_terms")
-        || url.searchParams.get("highlight")
-        || "";
-    localStorage.removeItem("sphinx_highlight_terms")
-    url.searchParams.delete("highlight");
-    window.history.replaceState({}, "", url);
-
-    // get individual terms from highlight string
-    const terms = highlight.toLowerCase().split(/\s+/).filter(x => x);
-=======
       localStorage.getItem("sphinx_highlight_terms")
       || url.searchParams.get("highlight")
       || "";
@@ -139,7 +92,6 @@ const SphinxHighlight = {
       .toLowerCase()
       .split(/\s+/)
       .filter((x) => x);
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
     if (terms.length === 0) return; // nothing to do
 
     // There should never be more than one element matching "div.body"
@@ -155,19 +107,11 @@ const SphinxHighlight = {
       document
         .createRange()
         .createContextualFragment(
-<<<<<<< HEAD
-          '<p class="highlight-link">' +
-            '<a href="javascript:SphinxHighlight.hideSearchWords()">' +
-            _("Hide Search Matches") +
-            "</a></p>"
-        )
-=======
           '<p class="highlight-link">'
             + '<a href="javascript:SphinxHighlight.hideSearchWords()">'
             + _("Hide Search Matches")
             + "</a></p>",
         ),
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
     );
   },
 
@@ -181,11 +125,7 @@ const SphinxHighlight = {
     document
       .querySelectorAll("span.highlighted")
       .forEach((el) => el.classList.remove("highlighted"));
-<<<<<<< HEAD
-    localStorage.removeItem("sphinx_highlight_terms")
-=======
     localStorage.removeItem("sphinx_highlight_terms");
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
   },
 
   initEscapeListener: () => {
@@ -194,12 +134,6 @@ const SphinxHighlight = {
 
     document.addEventListener("keydown", (event) => {
       // bail for input elements
-<<<<<<< HEAD
-      if (BLACKLISTED_KEY_CONTROL_ELEMENTS.has(document.activeElement.tagName)) return;
-      // bail with special keys
-      if (event.shiftKey || event.altKey || event.ctrlKey || event.metaKey) return;
-      if (DOCUMENTATION_OPTIONS.ENABLE_SEARCH_SHORTCUTS && (event.key === "Escape")) {
-=======
       if (BLACKLISTED_KEY_CONTROL_ELEMENTS.has(document.activeElement.tagName))
         return;
       // bail with special keys
@@ -209,7 +143,6 @@ const SphinxHighlight = {
         DOCUMENTATION_OPTIONS.ENABLE_SEARCH_SHORTCUTS
         && event.key === "Escape"
       ) {
->>>>>>> 6ef2fbfca306e754ecaf6b0145c837157367ad75
         SphinxHighlight.hideSearchWords();
         event.preventDefault();
       }
